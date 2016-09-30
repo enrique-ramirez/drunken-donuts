dbefecaa72797ff1e395ad5016367cb3d8fb6dd1
Gilberto Avalos <avalosagnostic@gmail.com>
2016-09-30 15:18:53 -0500

    Backend fixces

--- app.js
+++ app.js
@@ -17,12 +17,19 @@ var expressSession = session({
   saveUninitialized: true,
   cookie: { secure: true }
 });
+app.use(expressSession);
 
 io.use(function(socket, next) {
   expressSession(socket.request, socket.request.res, next);
 });
 
 app.get('/admin*', function(req, res) {
+  req.session.user = {
+    id: new Date().getTime(),
+    first_name: 'Gilberto',
+    role: 'support'
+  };
+
   res.sendFile(__dirname + '/public/admin.html');
 });
 
@@ -30,35 +37,30 @@ app.get('/chat*', function(req, res) {
   res.sendFile(__dirname + '/public/chat.html');
 });
 
-app.post('/login', function(req, res) {
-  req.session.user = {
-    username: req.body.username,
-    role: 'support'
-  };
-
-  res.json({ success: true });
-});
-
 var chats = [];
 io.on('connection', function (socket) {
   io.emit('updateChats', chats);
 
   socket.on('message', function(data) {
-    if (io.sockets.connected[data.clientId]) {
-      data.date = new Date();
-      io.sockets.connected[data.clientId].emit('message', data);
+    data.date = new Date();
+    io.emit('message', data);
 
-      var chat = _.find(chats, function(chat) {
-        return chat.clientId === chat.clientId;
-      });
+    // if (socket.request.session.user && socket.request.session.user.role !== 'support') {
+    //   var chat = _.find(chats, function(chat) {
+    //     return chat.clientId === chat.clientId;
+    //   });
 
-      if (chat) {
-        chat.messages.push(data);
-      }
-    }
+    //   if (chat) {
+    //     chat.messages.push(data);
+    //   }
+    // }
   });
 
   socket.on('clientConnected', function(data) {
+    if (data.user.role === 'user') {
+      socket.request.session.user = data;
+    }
+
     data = data || {};
 
     data.clientId = socket.id;
