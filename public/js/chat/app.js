8c141df8fa831c82f6ec7daf32df2c7ed8b89392
Enrique Ramírez <me@enrique-ramirez.com>
2016-09-30 15:24:51 -0500

    Remove hack.

--- public/js/chat/app.js
+++ public/js/chat/app.js
@@ -84,6 +84,4 @@ jQuery(document).ready(function() {
         $messages.append(html);
         scrollToBottom();
     }
-
-    initData({data: JSON.parse('{"user":{"id":"57ed819350af16c1c6000044","email":"enrique@wepow.com","first_name":"Enrique","last_name":"Ramírez","time_zone":"Guadalajara","language":"en","notification_settings":{"application_completed":"instant","candidate_messaged_recruiter":"instant","evaluation_completed":true,"comment_created":true,"candidate_direct_messaged_recruiter":true},"current_date":"2016-09-30T15:02:21.485"},"organization":{"id":"57ed817950af16c1c600001b","name":"WePow","subdomain":"dev","omniauth_providers":["facebook","gplus","linkedin"],"recruiters_can_request_phone_numbers":true,"created_at":"2016-09-29T16:02:49.658"}}')});
 });
